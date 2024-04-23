139b44e07186355da29f4255d4608e272a8483a3
Vishal Chintakunta <vkchntkn@memphis.edu>
2024-04-23 02:32:36 -0500

    Complete Django StoreApp

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+def product_index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+def product_show(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'myapp/show.html', {'product': product})
